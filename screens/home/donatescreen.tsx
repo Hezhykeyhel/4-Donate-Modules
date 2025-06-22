3d08350ea295dd6ef57cbbf6a7f558409f07ee5d
Egbetayo Damilola <76609799+Hezhykeyhel@users.noreply.github.com>
2025-06-22 15:37:28 +0100

    feat: donate modules complete

---
+++ screens/home/donatescreen.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@/components/Restyle";
+import React from "react";
+
+const DonateScreen = () => {
+  return (
+    <Box alignItems="center" justifyContent="center" flex={1}>
+      <Text variant="bold16">Donate Screen</Text>
+    </Box>
+  );
+};
+
+export default DonateScreen;
